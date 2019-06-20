b6c5e0b2e334467eab0df4c764c31a80c7861a2f
Anastasiia Smirnova <asmirnova@playtika.com>
2019-06-20 11:05:23 +0300

    Extend ServiceInstanceChooser with new choose method

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/ServiceInstanceChooser.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/ServiceInstanceChooser.java
@@ -33,4 +33,12 @@ public interface ServiceInstanceChooser {
 	 */
 	ServiceInstance choose(String serviceId);
 
+	/**
+	 * Chooses a ServiceInstance from the LoadBalancer for the specified service and according to the specified hint.
+	 * @param serviceId The service ID to look up the LoadBalancer.
+	 * @param hint to specify the service instance
+	 * @return A ServiceInstance that matches the serviceId.
+	 */
+	ServiceInstance choose(String serviceId, Object hint);
+
 }
